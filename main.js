a44abe60785b3718f8be939744aa99933d94eb4c
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-21 23:01:17 +0900

    JavaScript第2章 イベント 課題

--- main.js
+++
@@ -1,9 +0,0 @@
-let box = document.getElementById('box');
-let btn = document.getElementById('btn');
-
-btn.addEventListener("click" , function(){
-    box.style.backgroundColor = 'red';
-} , false); 
-
-btn.addEventListener("click" , function(){
-    alert('ボタンが押されました')} , false);
